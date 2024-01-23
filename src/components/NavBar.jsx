ff3a741f16ada5f98fb53de0605728a83d478be5
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-23 13:16:48 -0500

    menu items

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -49,7 +49,7 @@ export default function CustomAppBar() {
             <MenuItem onClick={handleMenuClose}>Chest</MenuItem>
             <MenuItem onClick={handleMenuClose}>Back</MenuItem>
             <MenuItem onClick={handleMenuClose}>Legs</MenuItem>
-            <MenuItem onClick={handleMenuClose}>Option 3</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Arms / Shoulders</MenuItem>
             <MenuItem onClick={handleMenuClose}>Option 3</MenuItem>
           </Menu>
         </Toolbar>
