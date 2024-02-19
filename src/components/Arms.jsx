dc5615ecc4be67c9f2ec14f811f863e416df523d
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-19 15:48:38 -0500

    arms

--- src/components/Arms.jsx
+++ src/components/Arms.jsx
@@ -1,6 +1,11 @@
-import * as React from 'react';
+import React, {useEffect} from 'react';
 
 export default function Arms(){
+    useEffect(async () => {
+        const exercises = await fetch("http://localhost:3000/arms")
+        console.log(exercises)
+    })
+
     return (
         <div>
             <h1>Arms page</h1>
